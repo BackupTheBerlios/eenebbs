ce0d103c301954660351a95f6d1196d5f1098d1b
chiller255 <chiller255>
2003-04-26 02:07:28 +0000

    inserts default user preferences; inserts default (0) message pointers for each users

--- eene/signup_validate.php
+++ eene/signup_validate.php
@@ -33,21 +33,38 @@ $sql_insert_stats = "INSERT INTO stats (user_id, logins, first_login, last_login
 		id AS user_id, 1 AS logins, NOW() AS first_login, NOW() AS last_login FROM users WHERE 
 		alias = '" . $req['alias'] . "'";
 
+$id = getUserID($req['alias']);
+
+$sql_get_all_prefs = "SELECT p.default, p.id FROM preferences p";
+$sth_get_all_prefs = @mysql_query($sql_get_all_prefs);
+$sql_insert_prefs = "INSERT INTO user_preferences (user_id, pref_id, value) VALUES ";
+while ($row_get_all_prefs = @mysql_fetch_assoc($sth_get_all_prefs)) {
+	$sql_insert_prefs .= "(" . $id . ", " . $row_get_all_prefs['id'] . ", " . $row_get_all_prefs['default'] . "), ";
+}
+$sql_insert_prefs = rtrim($sql_insert_prefs);
+$sql_insert_prefs = substr($sql_insert_prefs, 0, -1); 
+
+$sql_get_subs = "SELECT id FROM subs";
+$sth_get_subs = mysql_query($sql_get_subs);
+$sql_insert_ptrs = "INSERT INTO pointers (user_id, sub_id) VALUES ";
+while ($row_get_subs = mysql_fetch_assoc($sth_get_subs)) {
+	$sql_insert_ptrs .= "(" . $id . ", " . $row_get_subs['id'] . "), ";
+}
+$sql_insert_ptrs = rtrim($sql_insert_ptrs);
+$sql_insert_ptrs = substr($sql_insert_ptrs, 0, -1); 
+
 @mysql_query($sql_insert_stats);
-
-/* add user to 'prefs' table
-$sql_insert_prefs = "INSERT INTO preferences (user_id, display_automessage) SELECT id AS 
-		user_id, 'Y' AS display_automessage FROM users WHERE alias = '" . $req['alias'] . "'";
-
 @mysql_query($sql_insert_prefs);
-*/
+@mysql_query($sql_insert_ptrs);
+
+myLog('NEWUSER', $_SESSION['id']);
+myLog('LOGIN', $_SESSION['id']);
 
 $_SESSION['alias'] = $req['alias'];
 $_SESSION['sl'] = DEFAULT_SL;
 $_SESSION['logged_in'] = 1;
 $_SESSION['id'] = getUserID($req['alias']);
-myLog('NEWUSER', $_SESSION['id']);
-myLog('LOGIN', $_SESSION['id']);
+$_SESSION['sub'] = 1;
 
 header("Location: http://".$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/login.php?new=true");
 exit;
