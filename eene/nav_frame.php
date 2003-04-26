8484f84e89bd32bbace9269cf7d2548ea42c6617
chiller255 <chiller255>
2003-04-26 08:59:08 +0000

    added link to add a sub, sorted the list, fixed some extra-spaces problems, added the eeneBBS link at the bottom which reads info from config.php

--- eene/nav_frame.php
+++ eene/nav_frame.php
@@ -16,7 +16,7 @@ authenticate();
 
 <body class="navbar">
 <form name="navbar" id="navbar" method="post" action="main.php" target="mainFrame">
-	<p><strong> 
+	<p align="center"><strong> 
 		<?= BBSNAME ?>
 		</strong></p>
 	<table width="95%" border="0" cellspacing="0" cellpadding="0" align="center">
@@ -24,26 +24,27 @@ authenticate();
 			<td bgcolor="#BBBBBB"> 
 				<table width="100%" border="0" cellspacing="1" cellpadding="4">
 					<tr> 
-						<td class="navbarTable"><a href="main.php?newscan=true" target="mainFrame"><strong>Newscan 
+						<td nowrap="nowrap" class="navbarTable"><a href="main.php?newscan=true" target="mainFrame"><strong>Newscan 
 							Next Sub</strong></a></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><strong><a href="post.php?sub=<?= $_SESSION['sub'] ?>" target="mainFrame">Post a Message</a></strong></td>
+						<td nowrap="nowrap" class="navbarTable"><strong><a href="post.php?sub=<?= $_SESSION['sub'] ?>" target="mainFrame">Post 
+							a Message</a></strong></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><a href="main.php?order=asc" target="mainFrame">Read 
+						<td nowrap="nowrap" class="navbarTable"><a href="main.php?order=asc" target="mainFrame">Read 
 							Sub Forwards</a></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><a href="main.php?order=desc" target="mainFrame">Read 
+						<td nowrap="nowrap" class="navbarTable"><a href="main.php?order=desc" target="mainFrame">Read 
 							Sub Backwards</a></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><select name="sub" size="8" class="msgTable" style="cursor:pointer;cursor:hand;" onchange="javascript:document.navbar.submit()">
+						<td nowrap="nowrap" class="navbarTable"> <select name="sub" size="8" class="msgTable" style="cursor:pointer;cursor:hand;" onchange="javascript:document.navbar.submit()">
 								<?php
 $sth_get_subs = getSubs();
 while ($row_get_subs = mysql_fetch_assoc($sth_get_subs)) {
-	$option = (strlen($row_get_subs['name']) > 16) ? substr($row_get_subs['name'], 0, 16)
+	$option = (strlen($row_get_subs['name']) > 18) ? substr($row_get_subs['name'], 0, 18)
 			. '...' : $row_get_subs['name'];
 	if ($_SESSION['sub'] == $row_get_subs['id']) {
 ?>
@@ -59,44 +60,57 @@ while ($row_get_subs = mysql_fetch_assoc($sth_get_subs)) {
 							</select> </td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><a href="motto.php" target="mainFrame">Add 
+						<td nowrap="nowrap" class="navbarTable"><a href="motto.php" target="mainFrame">Add 
 							a Motto</a></td>
 					</tr>
-					<tr> 
-						<td class="navbarTable"><a href="users.php" target="mainFrame">User 
-							List</a></td>
+					<tr>
+						<td nowrap="nowrap" class="navbarTable"><a href="addsub.php" target="mainFrame">Add a 
+							Sub</a></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><a href="stats.php" target="mainFrame">Top 
+						<td nowrap="nowrap" class="navbarTable"><a href="automessage.php" target="mainFrame">AutoMessage</a></td>
+					</tr>
+					<tr> 
+						<td nowrap="nowrap" class="navbarTable"><a href="feedback.php" target="mainFrame">Feedback 
+							to Sysop</a> </td>
+					</tr>
+					<tr> 
+						<td nowrap="nowrap" class="navbarTable"><a href="prefs.php" target="mainFrame">Preferences</a></td>
+					</tr>
+					<tr> 
+						<td nowrap="nowrap" class="navbarTable"><a href="stats.php" target="mainFrame">Top 
 							Statistics</a></td>
 					</tr>
 					<tr> 
-						<td class="navbarTable"><a href="voting_booth.php" target="mainFrame">Voting 
+						<td nowrap="nowrap" class="navbarTable"><a href="users.php" target="mainFrame">User 
+							List</a></td>
+					</tr>
+					<tr> 
+						<td nowrap="nowrap" class="navbarTable"><a href="voting_booth.php" target="mainFrame">Voting 
 							Booth</a></td>
 					</tr>
-					<tr> 
-						<td class="navbarTable"><a href="prefs.php" target="mainFrame">Preferences</a></td>
-					</tr>
-					<tr> 
-						<td class="navbarTable"><a href="automessage.php" target="mainFrame">AutoMessage</a></td>
-					</tr>
-					<tr> 
-						<td class="navbarTable"><a href="feedback.php" target="mainFrame">Feedback 
-							to Sysop</a> </td>
-					</tr>
-<?php
+					<?php
 if ($_SESSION['sl'] == SYSOP_SL) {
 ?>
-<tr><td class="navbarTable"><a href="sysop.php" target="mainFrame">Sysop Menu</a></td></tr>
-<?php
+					<tr> 
+						<td nowrap="nowrap" class="navbarTable"><a href="sysop.php" target="mainFrame">Sysop 
+							Menu</a></td>
+					</tr>
+					<?php
 }
 ?>
 					<tr> 
-						<td class="navbarTable"><a href="logout.php" target="_top">Logout</a></td>
+						<td class="navbarTable"><a href="logout.php" target="_top">Logout 
+							<strong> 
+							<?= $_SESSION['alias'] ?>
+							</strong> </a></td>
 					</tr>
 				</table></td>
 		</tr>
 	</table>
+	<p class="eene"><a href="<?= EENELINK ?>" target="_top">eeneBBS</a> 
+		v<?= VERSION ?>
+	</p>
 </form>
 </body>
 </html>
