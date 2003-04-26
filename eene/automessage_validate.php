e9c3cb4b7a640479e76d7ad66349876053e847a9
chiller255 <chiller255>
2003-04-26 08:56:47 +0000

    fixed a parse error

--- eene/automessage_validate.php
+++ eene/automessage_validate.php
@@ -14,9 +14,8 @@ foreach ($_POST as $name => $value)
 $sql_insert_automessage = "INSERT INTO automessages (automessage, user_id, date) VALUES ('" .
 	$automessage . "', " . $_SESSION['id'] . ", NOW())";
 if (@mysql_query($sql_insert_automessage)) {
-	incrementStat($_SESSION['id'], 'automessages')
-	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 
-		"/automessage.php?success=true");
+	incrementStat($_SESSION['id'], 'automessages');
+	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/automessage.php?success=true");
 	exit;
 } else {
 	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 
