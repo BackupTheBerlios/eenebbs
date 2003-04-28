0aaadbef4928b818e9e4bd13f954e19737977b96
chiller255 <chiller255>
2003-04-28 21:27:32 +0000

    logs stuff; removed some commented-out lines

--- eene/post_validate.php
+++ eene/post_validate.php
@@ -13,15 +13,12 @@ if ($message == '' or !$message) {
 	exit;
 }
 
-/*
-foreach ($_POST as $name => $value) 
-	$req[$name] = trim(clean($value, 255));
-*/
 
 $sql_post = "INSERT INTO messages (sub_id, user_id, message, date) VALUES (" . $_SESSION['sub'] .
 		", " . $_SESSION['id'] . ", '" . $message . "', NOW())";
 if (@mysql_query($sql_post)) {
 	incrementStat($_SESSION['id'], 'posts');
+	myLog('POST', $_SESSION['id'], $_SESSION['sub']);
 	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 
 		"/main.php?success=true&newscan=true&current=true&sub=" . $_SESSION['sub']);
 	exit;
