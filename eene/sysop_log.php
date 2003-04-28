f95d5dc9055e222a765984216c26b6f53d64945c
chiller255 <chiller255>
2003-04-28 21:31:09 +0000

    uses .main style; reformatted output

--- eene/sysop_log.php
+++ eene/sysop_log.php
@@ -13,49 +13,57 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 <head>
 <title>Untitled Document</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
+<body class="main">
 <p><a href="sysop.php">back to sysop</a></p>
 <p>ok here is a log. this should include querying options on a startdate/enddate 
 	basis, query by user. we may want to make a notification about potential haxoring 
-	as this information is recorded.</p>
+	as this information is recorded.. since we have a record of bad passwords.</p>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td class="bgTable"><table cellpadding="4" cellspacing="1" width="100%">
+				<tr class="msgTitle"> 
+					<td>Date</td>
+					<td>Event ID</td>
+					<td>Type</td>
+					<td>Description</td>
+					<td>User</td>
+					<td>Note</td>
+				</tr>
+				<?php
 
-<table border=1>
-	<tr> 
-		<td><strong>Date</strong></td>
-		<td><strong>Type</strong></td>
-		<td><strong>Description</strong></td>
-		<td><strong>User</strong></td>
-		<td><strong>Note</strong></td>
-	</tr>
-	<?php
-
-$sql_get_log = "SELECT l.date, l.event_id, e.descr, u.alias, l.note FROM log l,
+$sql_get_log = "SELECT l.date, l.event_id, e.short_descr, e.descr, u.alias, l.note FROM log l,
 		event_ids e, users u WHERE u.id = l.user_id AND e.id = l.event_id ORDER BY l.date DESC";
 $sth_get_log = @mysql_query($sql_get_log);	
 while ($row = mysql_fetch_assoc($sth_get_log)) {
 ?>
-	<tr> 
-		<td> 
-			<?= $row['date'] ?>
-		</td>
-		<td> 
-			<?= $row['event_id'] ?>
-		</td>
-		<td> 
-			<?= $row['descr'] ?>
-		</td>
-		<td> 
-			<?= $row['alias'] ?>
-		</td>
-		<td> 
-			<?= $row['note'] ?>
-		</td>
-	</tr>
-	<?php
+				<tr class="msgText"> 
+					<td> 
+						<?= $row['date'] ?>
+					</td>
+					<td> 
+						<?= $row['event_id'] ?>
+					</td>
+					<td>
+						<?= $row['short_descr'] ?>
+					</td>
+					<td> 
+						<?= $row['descr'] ?>
+					</td>
+					<td> 
+						<?= $row['alias'] ?>
+					</td>
+					<td> 
+						<?= $row['note'] ?>
+					</td>
+				</tr>
+				<?php
 }
 ?>
+			</table></td>
+	</tr>
 </table>
 </body>
 </html>
--- eene/users.php
+++ eene/users.php
@@ -12,50 +12,64 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 <head>
 <title>Untitled Document</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
+<body class="main">
 
 <p>a user list</p>
-<table border=1>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
-		<td>Alias</td>
-		<td>Location</td>
-		<td>Website</td>
-		<td>Security Level</td>
-		<td>Avatar</td>
-		<td>User Since</td>
-	</tr>
-<?php
+		<td class="bgTable">
+			<table cellpadding="4" cellspacing="1" width="100%">
+				<tr class="msgTitle"> 
+					<td>Alias</td>
+					<td>Location</td>
+					<td>Website</td>
+					<td>Security Level</td>
+					<td>User Since</td>
+				</tr>
+				<?php
 
-$sql_users = "SELECT u.alias, u.location, u.email, s.descr, u.site, u.avatar, 
+$sql_users = "SELECT u.alias, u.location, u.email, s.descr, u.site,
 		UNIX_TIMESTAMP(st.first_login) FROM stats st, users u, security_levels s WHERE 
 		s.sl = u.sl AND st.user_id = u.id ORDER BY s.sl DESC, u.alias";
 $users = @mysql_query($sql_users);
 
 while ($row = mysql_fetch_assoc($users)) {
 ?>
-	<tr>
-		<td>
-<?php
+				<tr class="msgTable"> 
+					<td> 
+						<?php
 	if ($row['email'] != '') {
 ?>
-		<a href="mailto:<?= $row['email'] ?>"><?= $row['alias'] ?></a>
-<?php
+						<a href="mailto:<?= $row['email'] ?>"> 
+						<?= $row['alias'] ?>
+						</a> 
+						<?php
 	} else {
 ?>
-		<?= $row['alias'] ?>
-<?php
+						<?= $row['alias'] ?>
+						<?php
 	}
-?>		
-		</td>
-		<td><?= $row['location'] ?></td>
-		<td><a href="<?= $row['site'] ?>"><?= $row['site'] ?></a></td>
-		<td><?= $row['descr'] ?></td>
-		<td><img src="<?= $row['avatar'] ?>" /></td>
-		<td><?= date("F j, Y", $row['UNIX_TIMESTAMP(st.first_login)']) ?></td>
+?>
+					</td>
+					<td> 
+						<?= $row['location'] ?>
+					</td>
+					<td><a href="http://<?= $row['site'] ?>"> 
+						<?= $row['site'] ?>
+						</a></td>
+					<td> 
+						<?= $row['descr'] ?>
+					</td>
+					<td> 
+						<?= date("F j, Y", $row['UNIX_TIMESTAMP(st.first_login)']) ?>
+					</td>
+				</tr>
+				<?php } ?>
+			</table></td>
 	</tr>
-<?php } ?>
 </table>
 </body>
 </html>
