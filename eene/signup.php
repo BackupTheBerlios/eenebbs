1ded5157f4674806a7f1cdb59c30435151660bbb
chiller255 <chiller255>
2003-04-28 21:28:53 +0000

    uses .main syle; removed stuff about avatars/tinyheads

--- eene/signup.php
+++ eene/signup.php
@@ -10,14 +10,16 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 <head>
 <title>eeneBBS : signup</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+
 </head>
 
-<body>
+<body class="main">
 <p><a href="index.php">back to index</a></p>
 <p>sure looks to me like you want to sign up for the <strong>eeneBBS</strong> 
 	demo. fill out this form.</p>
 <form name="signup" id="signup" method="post" action="signup_validate.php">
-	<p>these two fields are <em>required</em>:<br />
+	<p>these three fields are <em>required</em>:<br />
 		<br />
 		<strong>alias:</strong> 
 		<input name="alias" type="text" id="alias" />
@@ -25,20 +27,19 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 		<strong>password:</strong> 
 		<input name="password" type="password" id="password" />
 		<br />
+		<strong>password (again)</strong>: 
+		<input name="password2" type="password" id="password2" />
+		<br />
 		<br />
 		the following fields are <em>optional</em>:<br />
 		<br />
 		<strong>email address</strong> (public): 
 		<input name="email" type="text" id="email" />
 		<br />
-		<strong>off-site link to avatar</strong> (to be displayed with your message--<br />
-		must be GIF or JPG smaller than or equal to 64x64 pixels): 
-		<input name="avatar" type="text" id="avatar" />
-		<br />
-		<strong>web site</strong> (public): 
+		<strong>web site</strong> (public): http:// 
 		<input name="site" type="text" id="site" />
 		<br />
-		<strong>location</strong> (public; appears next to your name in userlist): 
+		<strong>location</strong> (public): 
 		<input name="location" type="text" id="location" />
 	</p>
 	<p>
