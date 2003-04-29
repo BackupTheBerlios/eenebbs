cb885feec11b0ab16f7410f4b88b0b87fa0d4b53
chiller255 <chiller255>
2003-04-29 06:02:02 +0000

    posts WITH TAGLINES

--- eene/post_validate.php
+++ eene/post_validate.php
@@ -6,6 +6,10 @@ require_once 'lib/config.php';
 session_start();
 authenticate();
 
+# this is kind of dumb
+define ( 'RANDOMLY', 5 );
+define ( 'LOOP', 6 );
+
 $message = trim(cleanAllowImg($_POST['message'], MAXMSGLENGTH));
 if ($message == '' or !$message) {
 	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 
@@ -13,9 +17,37 @@ if ($message == '' or !$message) {
 	exit;
 }
 
-
-$sql_post = "INSERT INTO messages (sub_id, user_id, message, date) VALUES (" . $_SESSION['sub'] .
-		", " . $_SESSION['id'] . ", '" . $message . "', NOW())";
+switch (getUserPrefs($_SESSION['id'], 'TAGLINES')) {
+	case RANDOMLY:
+		$sql_get_tagline = "SELECT id FROM taglines WHERE user_id = " . $_SESSION['id'] . " ORDER BY RAND() LIMIT 1";
+		$sth_get_tagline = @mysql_query($sql_get_tagline);
+		if ($sth_get_tagline and mysql_num_rows($sth_get_tagline) > 0) {
+			$row_get_tagline = @mysql_fetch_assoc($sth_get_tagline);
+			$tagline = $row_get_tagline['id'];
+		} 
+		break;
+	case LOOP:
+		$sql_get_tagline = "SELECT t.id FROM taglines t, users u WHERE u.last_tagline = t.id AND u.id = " . $_SESSION['id'];
+		$sth_get_tagline = @mysql_query($sql_get_tagline);
+		if ($sth_get_tagline and mysql_num_rows($sth_get_tagline) > 0) {
+			$row_get_tagline = @mysql_fetch_assoc($sth_get_tagline);
+			$tagline = $row_get_tagline['id'];
+		} else {
+			$sql_get_tagline = "SELECT t.id FROM taglines t WHERE user_id = " . $_SESSION['id'];
+			if ($sth_get_tagline and mysql_num_rows($sth_get_tagline) > 0) {
+				$row_get_tagline = @mysql_fetch_assoc($sth_get_tagline);
+				$tagline = $row_get_tagline['id'];
+			}
+		}
+ 		break; 
+}
+if ($tagline) 
+	$sql_post = "INSERT INTO messages (sub_id, user_id, message, date, tag_id) VALUES (" . $_SESSION['sub'] .
+			", " . $_SESSION['id'] . ", '" . $message . "', NOW(), " . $tagline . " )";
+else
+	$sql_post = "INSERT INTO messages (sub_id, user_id, message, date) VALUES (" . $_SESSION['sub'] .
+			", " . $_SESSION['id'] . ", '" . $message . "', NOW())";
+			
 if (@mysql_query($sql_post)) {
 	incrementStat($_SESSION['id'], 'posts');
 	myLog('POST', $_SESSION['id'], $_SESSION['sub']);
