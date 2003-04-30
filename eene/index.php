55dcd282ef23d4d8e0b57fdb5e53cb0bbc189d2b
chiller255 <chiller255>
2003-04-30 23:15:50 +0000

    fixed some more logout problems

--- eene/index.php
+++ eene/index.php
@@ -3,7 +3,17 @@ session_start();
 require_once 'lib/config.php';
 require_once 'lib/utils.php';
 
-if (isset($_SESSION['id'])) {
+if (isset($_GET['error']) and $_GET['error'] == 'You are now logged out.') {
+	myLog('LOGOUT', $_SESSION['id']);
+	$_SESSION['logged_in'] = null;
+	unset($_SESSION['logged_in']);
+	$_SESSION['alias'] = null;
+	unset($_SESSION['alias']);
+	$_SESSION['id'] = null;
+	unset($_SESSION['id']);
+	$_SESSION['sl'] = null;
+	unset($_SESSION['sl']);	
+} else if (isset($_SESSION['id'])) {
 	header("Location: http://" .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 
 			"/main_frames.php?login=true&sub=1");
 }
@@ -11,13 +21,7 @@ if (isset($_SESSION['id'])) {
 $sql_get_motto = "SELECT motto FROM mottos ORDER BY RAND() LIMIT 1";
 $row = @mysql_fetch_assoc(@mysql_query($sql_get_motto));
 
-if (isset($_GET['error']) and $_GET['error'] == 'You are now logged out.') {
-	myLog('LOGOUT', $_SESSION['id']);
-	$_SESSION['logged_in'] = null;
-	unset($_SESSION['logged_in']);
-	$_SESSION['alias'] = null;
-	unset($_SESSION['alias']);	
-}
+
 echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
