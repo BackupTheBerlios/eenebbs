2e7f104ca14668096051f124a604aac3e0ce8fef
chiller255 <chiller255>
2003-04-30 19:24:51 +0000

    this should be deleted and output redirected to login.php

--- eene/logout.php
+++ eene/logout.php
@@ -17,17 +17,16 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Untitled Document</title>
+<title><?= BBSNAME ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 <body class="main">
-<p>You have been logged out.</p>
+<p class="error">You have been logged out.</p>
 <form action="login.php" method="post" name="login" id="login">
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="0">
-	<tr>
-		<td class="bgTable">
-<table width="100%" border="0" cellspacing="1" cellpadding="8">
+  <table width="400" border="0" align="center" cellpadding="0" cellspacing="0">
+		<tr> 
+			<td class="bgTable"> <table width="100%" border="0" cellspacing="1" cellpadding="8">
 					<tr> 
 						<td colspan="2" nowrap="nowrap" class="splashBBS"> 
 							<?= BBSNAME ?>
@@ -37,32 +36,31 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">";
 						</td>
 					</tr>
 					<tr> 
-						<td colspan="3" class="msgText"> <div align="center"> 
+						<td colspan="3" class="tagline"> <div align="center"> 
 								<?= $row['motto']; ?>
 							</div></td>
 					</tr>
 					<tr> 
 						<td class="msgTitle">NN:</td>
-						<td width="100%" colspan="2" nowrap="nowrap" class="msgText"><input name="alias" type="text" id="alias2" size="16" maxlength="32" />
+						<td width="100%" colspan="2" nowrap="nowrap" class="msgTable"><input name="alias" type="text" id="alias2" size="16" maxlength="32" />
 							No account? <a href="signup.php">Sign up</a>! </td>
 					</tr>
 					<tr> 
 						<td class="msgTitle">PW:</td>
-						<td width="100%" colspan="2" class="msgText"><input name="password" type="password" id="password3" size="16" maxlength="32" /> 
+						<td width="100%" colspan="2" class="msgTable"><input name="password" type="password" id="password3" size="16" maxlength="32" /> 
 							<input name="login" type="submit" id="login3" value="Login" /> </td>
 					</tr>
 					<tr> 
-						<td class="eene"  style="background-color: #EEEEEE;">&nbsp;</td>
+						<td class="eene"  style="background-color: #EEEEEE;"></td>
 						<td width="100%" class="eene" style="background-color: #EEEEEE;"><a href="http://eenebbs.berlios.de">eeneBBS 
 							<?= VERSION ?>
 							</a> </td>
 						<td class="eene"  style="background-color: #EEEEEE;">This site requires 
 							a frames-capable browser.</td>
 					</tr>
-				</table>
-			</td>
-	</tr>
-</table>
+				</table></td>
+		</tr>
+	</table>
   <p>&nbsp; </p>
 </form>
 </body>
