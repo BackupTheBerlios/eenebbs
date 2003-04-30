0a7a065d213f6115f8728776ddc91496c88126b3
chiller255 <chiller255>
2003-04-30 23:19:07 +0000

    fixed bugs

--- eene/voting_add_validate.php
+++ eene/voting_add_validate.php
@@ -10,7 +10,12 @@ foreach ($_POST as $name => $value)
 	$req[$name] = trim(clean($value, 255));
 	
 $sql_add_topic = "INSERT INTO voting_topics (name, date) VALUES ('" . $req['topic'] . "', NOW())";
-@mysql_query($sql_add_topic);
+if (!@mysql_query($sql_add_topic)) {
+	$_SESSION['error'] = "Add topic failed.";
+	header("Location: http://" . $_SERVER['HTTP_HOST'] . 	
+			dirname($_SERVER['PHP_SELF']) . "/voting_booth.php" );
+	exit;
+}
 
 $sql_get_id = "SELECT id FROM voting_topics WHERE name = '" . $req['topic'] . "'";
 $sth_get_id = @mysql_query($sql_get_id);
@@ -24,12 +29,18 @@ for ($i = 1; $i <= 10; $i++)
 
 $sql_add_options = rtrim($sql_add_options);
 $sql_add_options = substr($sql_add_options, 0, -1); 
+#debug($sql_add_options); exit;
 
-@mysql_query($sql_add_options);
-
-myLog('VTOPIC', $_SESSION['id'], $req['topic']);
-
-header("Location: http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/voting_booth.php?" );
-
-exit;
+if (@mysql_query($sql_add_options)) {
+	myLog('VTOPIC', $_SESSION['id'], $req['topic']);
+	$_SESSION['success'] = "Topic added!";
+	header("Location: http://" . $_SERVER['HTTP_HOST'] . 	
+			dirname($_SERVER['PHP_SELF']) . "/voting_booth.php" );
+	exit;
+} else {	
+	$_SESSION['error'] = "Add topic failed.";
+	header("Location: http://" . $_SERVER['HTTP_HOST'] . 	
+			dirname($_SERVER['PHP_SELF']) . "/voting_booth.php" );
+	exit;
+}
 ?>
